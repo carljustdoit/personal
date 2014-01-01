e0587cd99ffe9d828c38817e7c9631375f5c8468
Carl Liu <carljustdoit@gmail.com>
2013-12-31 23:47:20 -0800

    refined website

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Personal::Application.routes.draw do
-  get "pages/home"
+  root "pages#home"
+  get "about" => "pages#about"
+  get "projects" => "pages#projects"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
